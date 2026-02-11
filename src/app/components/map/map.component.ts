5319752bc91966c37654580305d3e75332762dfe
Thomas <castella.thomas30@gmail.com>
2026-02-11 11:48:39 +0100

    fix bug

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,6 +1,8 @@
 import { Component, AfterViewInit, OnDestroy, ViewChild, ElementRef, Inject, PLATFORM_ID, signal, effect } from '@angular/core';
 import { isPlatformBrowser } from '@angular/common';
 
+declare const L: any;
+
 @Component({
     selector: 'app-map',
     templateUrl: './map.component.html',
@@ -13,7 +15,7 @@ export class MapComponent implements AfterViewInit, OnDestroy {
 	private tileLayer: any;
 	private isDarkMode = signal(false);
 
-	constructor() {
+	constructor(@Inject(PLATFORM_ID) private platformId: Object) {
 		// Observer les changements de thème et mettre à jour la carte
 		effect(() => {
 			if (this.isDarkMode() && this.tileLayer) {
@@ -24,14 +26,11 @@ export class MapComponent implements AfterViewInit, OnDestroy {
 		});
 	}
 
-    constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
-
-    async ngAfterViewInit(): Promise<void> {
+    ngAfterViewInit(): void {
         if (isPlatformBrowser(this.platformId)) {
-            const L = await import('leaflet'); 
-            this.initMap(L);
+            this.initMap();
+            this.observeThemeChanges();
         }
-		this.observeThemeChanges();
     }
 
     ngOnDestroy(): void {
@@ -40,7 +39,7 @@ export class MapComponent implements AfterViewInit, OnDestroy {
         }
     }
 
-    private initMap(L: any) {
+    private initMap() {
         this.map = L.map(this.mapContainer.nativeElement).setView([48.00611, 0.19955], 13);
 		
 		// Charger le thème initial
