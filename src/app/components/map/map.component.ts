6ee895bdd9cc9de317046dabc07cae8b1ff3a145
Cabaret <noahcabaret0902@gmail.com>
2026-02-11 10:45:12 +0100

    correction map

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -1,35 +1,36 @@
-import { Component, AfterViewInit, OnDestroy, ViewChild, ElementRef } from '@angular/core';
-
-declare const L: any;
+import { Component, AfterViewInit, OnDestroy, ViewChild, ElementRef, Inject, PLATFORM_ID } from '@angular/core';
+import { isPlatformBrowser } from '@angular/common';
 
 @Component({
-	selector: 'app-map',
-	templateUrl: './map.component.html',
-	styleUrls: ['./map.component.scss'],
-	standalone: true,
+    selector: 'app-map',
+    templateUrl: './map.component.html',
+    styleUrls: ['./map.component.scss'],
+    standalone: true,
 })
-
 export class MapComponent implements AfterViewInit, OnDestroy {
-	@ViewChild('mapContainer', { static: true }) mapContainer!: ElementRef<HTMLDivElement>;
-	private map: any;
+    @ViewChild('mapContainer', { static: true }) mapContainer!: ElementRef<HTMLDivElement>;
+    private map: any;
 
-	ngAfterViewInit(): void {
-		this.initMap();
-	}
+    constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
 
-	ngOnDestroy(): void {
-		if (this.map) {
-			this.map.remove();
-		}
-	}
+    async ngAfterViewInit(): Promise<void> {
+        if (isPlatformBrowser(this.platformId)) {
+            const L = await import('leaflet'); 
+            this.initMap(L);
+        }
+    }
 
-	private initMap() {
+    ngOnDestroy(): void {
+        if (this.map) {
+            this.map.remove();
+        }
+    }
+
+    private initMap(L: any) {
         this.map = L.map(this.mapContainer.nativeElement).setView([48.00611, 0.19955], 13);
 
         L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
             attribution: '© OpenStreetMap contributors © CARTO',
         }).addTo(this.map);
-		
-	}
+    }
 }
-
