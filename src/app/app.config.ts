dc0379d4755b35e13a6e89bbcda9b814812ff60e
Thomas <castella.thomas30@gmail.com>
2026-03-10 11:09:40 +0100

    fix bug

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,7 +1,5 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
 import { provideRouter } from '@angular/router';
-import { provideHttpClient, withFetch } from '@angular/common/http';
-
 import { routes } from './app.routes';
 import { provideClientHydration, withEventReplay } from '@angular/platform-browser';
 import { provideHttpClient } from '@angular/common/http';
